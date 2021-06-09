af27813d442db29ac3c31db4353b33d873368606
arif <OrangeBlu20R>
2021-06-09 20:43:18 +1000

    Base

--- main.py
+++ main.py
@@ -1,62 +1,54 @@
 import matplotlib.pyplot as plt
-import numpy as np
-
+import random
+import math
 
 def celsius_to_fahrenheit(celsius):
     return (celsius * 9 / 5) + 32
 
 
-SAMPLE_SIZE = 100
+
 LEARNING_RATE = 0.0001
+celsius_values = range(-50,50)
+SAMPLE_SIZE = len(celsius_values)
+fahrenheit_values = [celsius_to_fahrenheit(i) for i in celsius_values]
 
-celsius_values = np.linspace(-50, 50, 100)
-celsius_values = celsius_values.reshape(SAMPLE_SIZE, 1)
-fahrenheit_values = celsius_to_fahrenheit(celsius_values)
-
-weights1 = np.random.randn(1, 1)
-biases1 = np.random.randn(1, 2)
-
-weights2 = np.random.randn(2, 1)
-biases2 = np.random.randn(1, 1)
+weight = random.random()
+bias = random.random()
 
 
 def forward():
-    output1 = np.dot(celsius_values, weights1) + biases1
-    output2 = np.dot(output1, weights2) + biases2
-    loss = np.square(output2 - fahrenheit_values).sum() / SAMPLE_SIZE
-    return output1, output2, loss
+    output = [i * weight + bias for i in celsius_values]
+    loss = sum([ math.pow(output[i] - fahrenheit_values[i], 2) for i in range(len(celsius_values))]) / SAMPLE_SIZE
+    return output, loss
 
 
-def backward(output1, output2):
-    grad_output2 = 2 * (output2 - fahrenheit_values) / SAMPLE_SIZE
-    grad_weights_2 = np.dot(output1.T, grad_output2)
-    grad_biases_2 = np.sum(grad_output2)
+def backward(output):
+    grad_output = [ 2*(output[i] - fahrenheit_values[i])/SAMPLE_SIZE for i in range(len(celsius_values))]
+    grad_weight = sum([output[i] * grad_output[i] for i in range(len(celsius_values))])
+    grad_bias = sum(grad_output)
 
-    grad_output1 = np.dot(grad_output2, weights2.T)
-    grad_weights_1 = np.dot(celsius_values.T, grad_output1)
-    grad_biases_1 = np.sum(grad_output1)
-
-    return grad_weights_1, grad_biases_1, grad_weights_2, grad_biases_2
+    return grad_weight, grad_bias
 
 
-def update_parameters(grad_weights_1, grad_biases_1, grad_weights_2, grad_biases_2):
-    global weights2, biases2, weights1, biases1
-    weights2 = weights2 - (LEARNING_RATE * grad_weights_2)
-    biases2 = biases2 - (LEARNING_RATE * grad_biases_2)
+def update_parameters(grad_weight, grad_bias):
+    global weight, bias
 
-    weights1 = weights1 - (LEARNING_RATE * grad_weights_1)
-    biases1 = biases1 - (LEARNING_RATE * grad_biases_1)
+    descent_grad_weight = LEARNING_RATE * grad_weight
+    descent_grad_bias = LEARNING_RATE * grad_bias
+
+    weight = weight - descent_grad_weight
+    bias = bias - descent_grad_bias
 
 
 def train():
-    for t in range(2000):
-        output1, output2, loss = forward()
-        grad_weights_1, grad_biases_1, grad_weights_2, grad_biases_2 = backward(output1, output2)
-        update_parameters(grad_weights_1, grad_biases_1, grad_weights_2, grad_biases_2)
+    for t in range(10000):
+        output, loss = forward()
+        grad_weight, grad_bias = backward(output)
+        update_parameters(grad_weight, grad_bias)
 
 
 def get_predicted_fahrenheit_values():
-    output1, prediction, loss = forward()
+    prediction, loss = forward()
     return prediction
 
 
